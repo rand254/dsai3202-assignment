94169c5901b03cf6cbfa7970cd3e181031df9c3b
Rand Alzoubi <60304966@udst.edu.qa>
2026-04-10 06:38:46 +0300

    Experiment 1: Sentiment features only

--- src/train.py
+++ src/train.py
@@ -46,7 +46,7 @@ def build_features(df):
     temp_df = df.drop(columns=to_drop, errors='ignore')
     
     # 2. Select ONLY numeric columns
-    X = temp_df.select_dtypes(include=['number'])
+    X = temp_df.filter(regex='^sentiment_')
     
     # --- ADD THIS LINE TO FIX THE ERROR ---
     X = X.fillna(0) 
